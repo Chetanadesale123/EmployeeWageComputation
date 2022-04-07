0ff4cbf2b37f60dafac66a554c6723a84bdf2216
59114_Chetana <deslechetna1999@gmail.com>
2022-04-07 18:17:24 +0530

    [chetana] Added : Employee Wage Using Switch Case

--- EmployeeWageComputation/EmployeeWage.cs
+++ EmployeeWageComputation/EmployeeWage.cs
@@ -8,7 +8,7 @@ namespace EmployeeWageComputation
 {
     public class EmployeeWage
     {
-        const int IS_PRESENT = 0, Wage_PER_HR = 20, FULL_DAY_HR = 8, PART_DAY_HR = 4, IS_FULL_DAY_PRESENT = 0, IS_PART_TIME_PRESENT = 1;
+        const int IS_PRESENT = 1, IS_ABSENT = 0, Wage_PER_HR = 20, FULL_DAY_HR = 8, PART_DAY_HR = 4, IS_FULL_DAY_PRESENT = 1, IS_PART_TIME_PRESENT = 2;
         int empHrs, totalEmpWage;
         public void EmployeeAttendence()
         {
@@ -24,28 +24,29 @@ namespace EmployeeWageComputation
         {
             Random random = new Random();
             int check = random.Next(0, 3);
-            if (check == IS_FULL_DAY_PRESENT)
+            switch (check)
             {
-                empHrs = FULL_DAY_HR;
-            }
-            else if(check == IS_PART_TIME_PRESENT)
-            {
-                empHrs = PART_DAY_HR;
-            }
-            else
-            {
-                empHrs = 0;
+                case IS_ABSENT:
+                    empHrs = 0;
+                    break;
+                case IS_FULL_DAY_PRESENT:
+                    empHrs = FULL_DAY_HR;
+                    break;
+                case IS_PART_TIME_PRESENT:
+                    empHrs = FULL_DAY_HR;
+                    break;
+
             }
             totalEmpWage = empHrs * Wage_PER_HR;
-            Console.WriteLine("Total Emp Wage for a day" + totalEmpWage);
+            Console.WriteLine("Total Emp Wage for a day"  +totalEmpWage);
 
 
         }
-        
-            
 
 
-            
+
+
+
 
     }
 }
