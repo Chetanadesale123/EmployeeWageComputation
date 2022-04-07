4de00131a3e13e32eb28c70e41bd541cf513bf56
59114_Chetana <deslechetna1999@gmail.com>
2022-04-07 23:14:44 +0530

    [chetana]Added:main method

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -1,5 +1,12 @@
-﻿using EmployeeWageComputation;
+﻿using System;
+namespace EmployeeWageComputation;
+class Program
+{
+    static void Main(string[] args)
+    {
 
-EmployeeWage computation = new EmployeeWage();
-computation.EmployeeAttendence();
-computation.CalculateEmpWage();
+        EmployeeWage computation = new EmployeeWage();
+        computation.EmployeeAttendence();
+        computation.CalculateEmpWage();
+    }
+}
